5e069516a0867159e9666682eb224745edc7901d
Taylor Ng <taylormng95@gmail.com>
2024-06-02 19:32:08 -0400

    modifies ternary operator in About.js and completes lab

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {props.bio == null || props.bio === '' ? null : <p>{props.bio}</p>}
+      {props.bio === undefined || props.bio === '' ? null : <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links github={props.github} linkedin={props.linkedin}/>
     </div>
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 function Links(props) {
-    console.log(props)
     return (
         <div>
             <h3>Links</h3>
